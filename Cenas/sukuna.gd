5efe83dcabd6efbb92c289561a7eb47201cc8610
RenanRabelo1 <renanrabelo82@gmail.com>
2025-09-20 10:18:02 -0300

    Knockback aplicado com sucesso

--- Cenas/sukuna.gd
+++ Cenas/sukuna.gd
@@ -1,39 +1,101 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var speed: float = 100.0
+@export var jump_velocity: float = -350.0
+var gravity: float = 980.0
+var is_attacking = false
 var is_agachando = false
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
+@onready var progress_bar: ProgressBar = $ProgressBar
+@onready var animacao = $Animacao_Sukuna
+@onready var timer_attack = $TimerAttack
+
+func _ready():
+	var project_gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+	if project_gravity:
+		gravity = project_gravity
+
+func _physics_process(delta):
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	move_and_slide()
+		velocity.y += gravity * delta
 	
+	if not is_attacking and not is_agachando:
+		handle_movement()
+	
+	update_animation()
+	move_and_slide()
+
+func handle_movement():
+	if Input.is_action_just_pressed("jump_sukuna") and is_on_floor():
+		velocity.y = jump_velocity
+	
+	if Input.is_action_just_released("jump_sukuna") and velocity.y < 0:
+		velocity.y *= 0.5
+	
+	var direction = Input.get_axis("left_sukuna", "right_sukuna")
+	
+	if direction != 0:
+		velocity.x = direction * speed
+		if direction > 0:
+			animacao.flip_h = false
+		else:
+			animacao.flip_h = true
+	else:
+		velocity.x = 0
+
+func start_attack():	
+	is_attacking = true
+	$TimerAttack.start()
+	$TimerColisaoAttack.start()
+
+func update_animation():
+	# Agachar
 	if Input.is_action_just_pressed("sukuna_agachar"):
 		$Hitbox/ColisaoDeCima.disabled = true
-		$Animacao_Sukuna.play("sukuna_agachando")
+		animacao.play("sukuna_agachando")
 		is_agachando = true
+		return
+	
+	if Input.is_action_just_released("sukuna_agachar"):
+		$Hitbox/ColisaoDeCima.disabled = false
+		is_agachando = false
+	
+	# Ataque
+	if Input.is_action_just_pressed("AttackSukunaUp") and not is_agachando:
+		animacao.play("Attack_Cima_Sukuna")
+		$AttackArea/ColisionShape.disabled = false
+		is_attacking = true
+		return
+	
+	# Outras animações
+	if not is_on_floor() && is_attacking == false && !is_agachando:
+		animacao.play("sukuna_jumping")
+	else:
+		if abs(velocity.x) > 0 && is_attacking == false && !is_agachando:
+			animacao.play("sukuna_andando")
+		else:
+			if is_attacking == false && !is_agachando:
+				animacao.play("idle")
 
 func take_damage():
-		$ProgressBar.value -= 1
-		print("Dano tomado! Vida: ", $ProgressBar.value)
+	progress_bar.value -= 1
+	print("Dano tomado! Vida: ", progress_bar.value)
+
+func _on_animacao_sukuna_animation_finished() -> void:
+	if animacao.animation == "Attack_Cima_Sukuna":
+		$AttackArea/ColisionShape.disabled = true
+		is_attacking = false
+	elif animacao.animation == "sukuna_agachando":
+		# Mantém agachado até soltar o botão
+		pass
 
 func _on_hitbox_area_entered(area: Area2D) -> void:
 	take_damage()
+	# CORREÇÃO: Passa a posição da área que causou o dano
+	apply_knockback(area.global_position)
 
-func _on_animacao_sukuna_animation_finished() -> void:
-	if $Animacao_Sukuna.animation == "sukuna_agachando":
-		$Hitbox/ColisaoDeCima.disabled = false
-		is_agachando = false
+# Função de knockback corrigida
+func apply_knockback(attacker_position: Vector2):
+	var direction = (global_position - attacker_position).normalized()
+	velocity = direction * 1000
+	move_and_slide()
