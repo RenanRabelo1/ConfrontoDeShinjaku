8bef5933997d21e54db3a6f39a82e66a494a0ab7
RenanRabelo1 <renanrabelo82@gmail.com>
2025-09-21 12:17:36 -0300

    Consertei agachar

--- Cenas/sukuna.gd
+++ Cenas/sukuna.gd
@@ -3,12 +3,10 @@ extends CharacterBody2D
 @export var speed: float = 100.0
 @export var jump_velocity: float = -350.0
 var gravity: float = 980.0
-var is_attacking = false
 var is_agachando = false
 
 @onready var progress_bar: ProgressBar = $ProgressBar
 @onready var animacao = $Animacao_Sukuna
-@onready var timer_attack = $TimerAttack
 
 func _ready():
 	var project_gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -19,7 +17,7 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
-	if not is_attacking and not is_agachando:
+	if is_agachando == false:
 		handle_movement()
 	
 	update_animation()
@@ -32,7 +30,7 @@ func handle_movement():
 	if Input.is_action_just_released("jump_sukuna") and velocity.y < 0:
 		velocity.y *= 0.5
 	
-	var direction = Input.get_axis("left_sukuna", "right_sukuna")
+	var direction = Input.get_axis("ui_left", "ui_right")
 	
 	if direction != 0:
 		velocity.x = direction * speed
@@ -43,58 +41,34 @@ func handle_movement():
 	else:
 		velocity.x = 0
 
-func start_attack():	
-	is_attacking = true
-	$TimerAttack.start()
-	$TimerColisaoAttack.start()
-
 func update_animation():
-	# Agachar
-	if Input.is_action_just_pressed("sukuna_agachar"):
-		$Hitbox/ColisaoDeCima.disabled = true
-		animacao.play("sukuna_agachando")
-		is_agachando = true
-		return
-	
-	if Input.is_action_just_released("sukuna_agachar"):
+	if Input.is_action_pressed("sukuna_agachar"):
+		if animacao.animation != "sukuna_agachando":
+			animacao.play("sukuna_agachando")
+			$Hitbox/ColisaoDeCima.disabled = true
+			is_agachando = true
+	elif Input.is_action_just_released("sukuna_agachar"):
 		$Hitbox/ColisaoDeCima.disabled = false
 		is_agachando = false
-	
-	# Ataque
-	if Input.is_action_just_pressed("AttackSukunaUp") and not is_agachando:
-		animacao.play("Attack_Cima_Sukuna")
-		$AttackArea/ColisionShape.disabled = false
-		is_attacking = true
-		return
-	
-	# Outras animações
-	if not is_on_floor() && is_attacking == false && !is_agachando:
-		animacao.play("sukuna_jumping")
-	else:
-		if abs(velocity.x) > 0 && is_attacking == false && !is_agachando:
+	elif not is_on_floor():
+		if animacao.animation != "sukuna_jumping":
+			animacao.play("sukuna_jumping")
+	elif abs(velocity.x) > 0 and not is_agachando:
+		if animacao.animation != "sukuna_andando":
 			animacao.play("sukuna_andando")
-		else:
-			if is_attacking == false && !is_agachando:
-				animacao.play("idle")
+	else:
+		if animacao.animation != "idle":
+			animacao.play("idle")
+
 
 func take_damage():
 	progress_bar.value -= 1
 	print("Dano tomado! Vida: ", progress_bar.value)
 
-func _on_animacao_sukuna_animation_finished() -> void:
-	if animacao.animation == "Attack_Cima_Sukuna":
-		$AttackArea/ColisionShape.disabled = true
-		is_attacking = false
-	elif animacao.animation == "sukuna_agachando":
-		# Mantém agachado até soltar o botão
-		pass
-
 func _on_hitbox_area_entered(area: Area2D) -> void:
 	take_damage()
-	# CORREÇÃO: Passa a posição da área que causou o dano
 	apply_knockback(area.global_position)
 
-# Função de knockback corrigida
 func apply_knockback(attacker_position: Vector2):
 	var direction = (global_position - attacker_position).normalized()
 	velocity = direction * 1000
